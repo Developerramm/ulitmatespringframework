7e413a3b55f3c6cef560447d74fe35fe8934805c
ram kumar <rr697144@gmail.com>
2025-11-19 22:23:47 +0530

    ioc container basic

---
+++ springcore/Proj1_IocContainer/src/main/java/com/app/beans/User.java
@@ -0,0 +1,13 @@
+package com.app.beans;
+
+public class User {
+	
+	public User() {
+		System.out.println("this is default constructor ");
+	}
+	
+	public void showUser() {
+		System.out.println("hello user");
+	}
+
+}
---
+++ springcore/Proj1_IocContainer/src/main/java/com/app/main/Test.java
@@ -0,0 +1,29 @@
+package com.app.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.beans.User;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = null;
+		context = new ClassPathXmlApplicationContext("config.xml");
+		
+		System.out.println(context);
+		System.out.println("Ram kumar maniyari");
+		
+		User u = context.getBean("user",User.class);
+		
+		
+		// this is manual object 
+		User user = new User();
+		user.showUser();
+		
+		// this is automatically created object 
+		u.showUser();
+		
+	}
+
+}
