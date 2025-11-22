f4a9a989d14beefcbb94e28bb75d65f782795eab
ram kumar <rr697144@gmail.com>
2025-11-22 14:37:39 +0530

    setter injection part 4

---
+++ springcore/Proj3_SetterInjection2/src/main/java/com/app/beans/Course.java
@@ -0,0 +1,14 @@
+package com.app.beans;
+
+// dependent class
+public class Course {
+	
+	public Course() {
+		System.out.println("Course.Course()");
+	}
+	
+	public void startCourse() {
+		System.out.println("Your course is loading....");
+	}
+
+}
