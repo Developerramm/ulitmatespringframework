a2f03eb0a5af6db9b09c559ecb2fdcfb4944a981
ram kumar <rr697144@gmail.com>
2025-11-24 13:04:35 +0530

    operator in java

---
+++ core/basic/src/operators/OperatorDemo.java
@@ -0,0 +1,31 @@
+package operators;
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+
+        int a = 4;
+        
+        int b = 6 + a;
+        System.out.println(b);
+
+        // System.out.println(true + true);
+
+        System.out.println(6 % 4);
+
+        System.out.println(10 * 4);
+
+        System.out.println(343 - 43);
+
+        System.out.println( 300 == 43);
+
+        System.out.println(43 != 43);
+        System.out.println(43==43);
+
+        System.out.println(43 > 89);
+
+        System.out.println(4343 >= 900);
+
+        System.out.println(43 > 10 && 30 < 490);
+        
+    }
+}
