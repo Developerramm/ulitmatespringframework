ca2f31ca9cdcc67b28e1ac0f62b8aa6a8c96eee3
ram kumar <rr697144@gmail.com>
2025-11-24 12:21:30 +0530

    literal in java

---
+++ core/basic/src/basic/Literal.java
@@ -0,0 +1,29 @@
+package basic;
+
+public class Literal {
+    public static void main(String[] args) {
+        System.out.println("Literal in java ");
+
+        byte age = 34;
+        System.out.println(age);
+
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.33d;
+
+        int age2 = 434;
+        System.out.println(age2);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+
+        short age3 = 63;
+        System.out.println(age3);
+
+        boolean a = true;
+        System.out.println(a);
+
+        String str = "Ram kumar";
+        System.out.println(str);
+    }
+}
