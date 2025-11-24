48fabcd8ff89917f4ea34f63569bad08792c738e
ram kumar <rr697144@gmail.com>
2025-11-24 12:00:09 +0530

    java variable

---
+++ core/basic/src/basic/Test.java
@@ -0,0 +1,37 @@
+package basic;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("This is test java class ");
+        int age = 30;
+        System.out.println(age);
+        String name = "Ram kumar";
+        System.out.println("Your name is " + name);
+
+        float marks = 343.34f;
+        System.out.println("your mark is " + marks);
+
+        double salary = 4324320;
+        System.out.println("Your salary is " + salary);
+
+        short number = 34;
+        System.out.println(number);
+
+        short number1 = 4342;
+        System.out.println(number1);
+
+        long number2 = 432434242432432l;
+        System.out.println(number2);
+
+        boolean isYoung = true;
+
+        if(isYoung){
+            System.out.println("You can vote now");
+        }else{
+            System.out.println("You can not vote here ");
+        }
+
+    }
+
+}
