9333164e8dfc48ea624c68eb571a877716583e26
ram kumar <rr697144@gmail.com>
2025-11-28 07:48:09 +0530

    treeset in collection framewrok

--- core/collectionFramework/src/TreeSetDemo.java
+++ core/collectionFramework/src/TreeSetDemo.java
@@ -1,7 +1,17 @@
+import java.util.TreeSet;
+
 public class TreeSetDemo {
 
     public static void main(String[] args) {
-        
+        TreeSet<String> t = new TreeSet<>();
+
+        t.add("A");
+        t.add("Z");
+        t.add("Y");
+        t.add("M");
+        t.add("H");
+
+        System.out.println(t);
     }
 
 }
