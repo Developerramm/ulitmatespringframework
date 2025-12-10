83594abe4a5f71b667a1bf2c9153fa53076d438b
ram kumar <rr697144@gmail.com>
2025-12-10 13:21:00 +0530

    object part1;

---
+++ core/basic/src/OOPs/Student.java
@@ -0,0 +1,11 @@
+package OOPs;
+
+public class Student {
+
+    int roll;
+    String name;
+    double per;
+
+    
+
+}
---
+++ core/basic/src/OOPs/UseStudent.java
@@ -0,0 +1,10 @@
+package OOPs;
+
+public class UseStudent {
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.getClass().getName());
+    }
+
+}
