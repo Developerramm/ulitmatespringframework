9addebbffdda1ebfbc7dcc2661a0a7dc617970e2
ram kumar <rr697144@gmail.com>
2025-12-14 09:10:04 +0530

    cpp basic syntax

---
+++ DSA/basic/mian.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    cout<< "Ram kumar " << endl;
+
+    cout << "Enter your name ";
+    string name;
+    cin >> name;
+    cout << "Your name is " << name << endl;
+
+    return 0;
+}
+
+
