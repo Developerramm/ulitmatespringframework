8602242ca64991ac20f1d25f5662b2a07e11bac8
ram kumar <rr697144@gmail.com>
2025-12-23 21:10:48 +0530

    cpp data type and variable

--- DSA/basic/mian.cpp
+++ DSA/basic/mian.cpp
@@ -2,12 +2,31 @@
 using namespace std;
 int main(){
 
-    cout<< "Ram kumar " << endl;
+    int a = 5;
+    cout << "Value of a is " << a << endl;
+    cout << sizeof(a) << endl;
 
-    cout << "Enter your name ";
-    string name;
-    cin >> name;
-    cout << "Your name is " << name << endl;
+    char ch = 'a';
+    cout << "Value of ch is " << ch << endl;
+    cout << sizeof(ch) << endl;
+
+    bool flag = true;
+    cout << "Value of flag is " << flag << endl;
+    cout << sizeof(flag) << endl;
+
+    float f = 1.2f;
+    cout << "Float value is " << f << endl;
+    cout << sizeof(f) << endl;
+
+    ch = 97;
+    cout << ch << endl;
+
+    // cout<< "Ram kumar " << endl;
+
+    // cout << "Enter your name ";
+    // string name;
+    // cin >> name;
+    // cout << "Your name is " << name << endl;
 
     return 0;
 }
