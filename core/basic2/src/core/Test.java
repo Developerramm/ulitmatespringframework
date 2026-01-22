9cb7cb7a6e72354a2af28e5ad7420c98a7510f11
ram kumar <rr697144@gmail.com>
2026-01-22 20:45:31 +0530

    predefine class as identifier

---
+++ core/basic2/src/core/Test.java
@@ -0,0 +1,19 @@
+package core;
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+        String String = "Ram kumar";
+        System.out.println(String);
+
+        int Exception = 20;
+        System.out.println(Exception);
+
+        int System = 30;
+        java.lang.System.out.println(System);
+
+
+    }
+
+}
