1246556d3f6fffbf426983827dbd7d1c248a400a
ram kumar <rr697144@gmail.com>
2026-02-02 12:51:25 +0530

    to string method

--- core/basic2/src/oops/Test.java
+++ core/basic2/src/oops/Test.java
@@ -7,6 +7,19 @@ class A{
     }
 
 }
+
+class Employee{
+    String id = "E133";
+    String name = "Ram kumar";
+    String salary = "4342435";
+    String address = "Noida";
+
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", address=" + address + "]";
+    }
+}
 public class Test {
 
     public static void main(String[] args) {
@@ -20,7 +33,25 @@ public class Test {
         System.out.println(a.toString());
 
         System.out.println(a);
+
+
+        Employee employee = new Employee();
+
+        System.out.println(employee);
+
+
+        String str = new String("Ram kumar maniayri");
+        System.out.println(str);
+
+        Integer integer = new Integer(24342);
+        System.out.println(integer);
         
+
+        Exception e = new Exception("My own Exception");
+        System.out.println(e);
+
+        Thread t = new Thread();
+        System.out.println(t);
     }
 
 }
