acc7ee1f21c580eff0e640103d7f4c75b0113ad7
ram kumar <rr697144@gmail.com>
2026-03-08 08:40:11 +0530

    member inner class

---
+++ core/basic2/src/inner/Test.java
@@ -0,0 +1,39 @@
+package inner;
+
+class A {
+    int i = 10;
+
+    void m1() {
+        System.out.println("A.m1()");
+    }
+
+    class B {
+        int j = 20;
+
+        void m2() {
+            System.out.println("A.B.m2()");
+        }
+    }
+}
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("ram kumar maniyari ");
+
+        A a = new A();
+
+        System.out.println(a.i);
+        a.m1();
+
+        A.B ab = new A().new B();
+
+        System.out.println(ab.j);
+        ab.m2();
+
+        A.B ab1 = a.new B();
+        System.out.println(ab1.j);
+        ab1.m2();
+    }
+
+}
