1c725487a670fca0e6e9f174c6b035eb19fee035
ram kumar <rr697144@gmail.com>
2026-03-17 14:23:25 +0530

    arrayIndexOutOfBound exception

---
+++ core/basic3/Exception_Handling_Basic/src/com/app/part1/Test1.java
@@ -0,0 +1,19 @@
+package com.app.part1;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Array index out of bound exception");
+		
+		
+		int[] arr = {10,2,4,5,8,9};
+		
+		System.out.println(arr);
+		
+		System.out.println(arr[5]);
+		
+		System.out.println(arr[10]);
+		
+	}
+
+}
