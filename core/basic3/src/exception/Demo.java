43c2c29e882ae97047e3c7d26532997724103603
ram kumar <rr697144@gmail.com>
2026-03-18 17:46:35 +0530

    instantiationException

---
+++ core/basic3/src/exception/Demo.java
@@ -0,0 +1,29 @@
+package exception;
+
+class Employee{
+	
+	static {
+		System.out.println("Employee class is loading.....");
+	}
+	
+	public Employee(int i) {
+		
+		System.out.println("Employee object is creating...");
+	}
+}
+
+public class Demo {
+	
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		System.out.println("Exception handling in java");
+		
+		Class c = Class.forName("exception.Employee");
+		
+		Object obj = c.newInstance();
+		
+		System.out.println(obj.getClass().getName());
+		
+		System.out.println(c.getClass().getName());
+	}
+
+}
